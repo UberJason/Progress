9076433816a2eb94070fd418c71721be5b37e49a
Jason Ji <uberjason@gmail.com>
2016-08-17 23:19:05 -0400

    Something I never got around to committing before...

--- Progress/ViewController.swift
+++ Progress/ViewController.swift
@@ -16,24 +16,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var chartView: LineChartView!
     
     var model: WeightModel?
+    let manager = HealthManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let manager = HealthManager()
-        
-        guard HKHealthStore.isHealthDataAvailable() else { print("No health data available."); return }
-        
-        manager.authorizeHealthSharingWithSuccess({
-            manager.getWeightData { (results: [HKQuantitySample]) -> Void in
-                self.model = WeightModel(rawValues: results)
-                dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                    self.loadGraph()
-                })
-            }
-            },
-            errorHandler: {
-                print("Health request denied.")
-        })
         
         hostingView.layer.cornerRadius = 4.0
         hostingView.clipsToBounds = true
@@ -43,7 +28,26 @@ class ViewController: UIViewController {
         
     }
     
-    func loadGraph() {
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard HKHealthStore.isHealthDataAvailable() else { print("No health data available."); return }
+        
+        manager.authorizeHealthSharingWithSuccess({
+            self.manager.getWeightData { (results: [HKQuantitySample]) -> Void in
+                self.model = WeightModel(rawValues: results)
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.initializeGraph()
+                    self.loadGraphData()
+                })
+            }
+            },
+            errorHandler: {
+                print("Health request denied.")
+        })
+    }
+    
+    func initializeGraph() {
         chartView.descriptionText = ""
         chartView.noDataText = "No data available - did you allow Health access?"
         chartView.dragEnabled = true
@@ -55,9 +59,6 @@ class ViewController: UIViewController {
         chartView.highlightPerDragEnabled = false
 
         chartView.legend.enabled = false
-        
-//        chartView.leftAxis.customAxisMax = 10
-//        chartView.leftAxis.customAxisMin = 0
         
         chartView.leftAxis.startAtZeroEnabled = false
         chartView.leftAxis.drawGridLinesEnabled = false
@@ -73,20 +74,11 @@ class ViewController: UIViewController {
         chartView.viewPortHandler.setMaximumScaleY(8.0)
         chartView.viewPortHandler.setMaximumScaleX(8.0)
         
+    }
+    
+    func loadGraphData() {
         let xVals = model?.chartXValues
         let yVals = model?.chartValues
-
-//        let yVals = [
-//            ChartDataEntry(value: 0, xIndex: 0),
-//            ChartDataEntry(value: 1, xIndex: 1),
-//            ChartDataEntry(value: 2, xIndex: 2),
-//            ChartDataEntry(value: 4, xIndex: 4),
-//            ChartDataEntry(value: 5, xIndex: 5),
-//            ChartDataEntry(value: 7, xIndex: 7),
-//            ChartDataEntry(value: 8, xIndex: 8),
-//            ChartDataEntry(value: 9, xIndex: 9),
-//        ]
-//        let xVals = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
         
         let set = LineChartDataSet(yVals: yVals)
         set.lineWidth = 1.0
